a284db69da6226e9f20f941092ed09fa05881562
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 19:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function playSound(soundFile) {
   var audio = new Audio("sounds/" + soundFile);
   audio.play();
   sounds.push(audio); // Store the Audio object in the array
+  document.getElementById('buttons').appendChild(audio); // Add the audio element to the HTML
 }
 
 function stopSound() {
   sounds.forEach(function(sound) {
     sound.pause();
     sound.currentTime = 0; // This line is to make sure the sound restarts when it is played again
+    document.getElementById('buttons').removeChild(sound); // Remove the audio element from the HTML
   });
+  sounds = []; // Clear the array
 }
